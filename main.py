c10b8a3f704bedf34e1b1a767162cf2fd71fde13
Mete Akyüz <akyuzmete0@gmail.com>
2025-02-20 01:26:22 +0300

    Player class added with basic functionality

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import pygame
-import constants
+from constants import *
+from player import *
+from circleshape import *
 def main():
   print("Starting asteroids!")
-  print(f"Screen width: {constants.SCREEN_WIDTH}")
-  print(f"Screen height: {constants.SCREEN_HEIGHT}")
+  print(f"Screen width: {SCREEN_WIDTH}")
+  print(f"Screen height: {SCREEN_HEIGHT}")
 
   pygame.init()
-  screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
+  screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+  
+  clock = pygame.time.Clock()
+
+  updatable_group = pygame.sprite.Group()
+  drawable_group = pygame.sprite.Group()
+  Player.containers = (updatable_group, drawable_group)
+  player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+  
+  dt = 0
   running = True
   while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+      if event.type == pygame.QUIT:
+        running = False
     
+    
+    
+    updatable_group.update(dt)
+
     screen.fill("black")
 
+    for drw in drawable_group:
+      drw.draw(screen)
+
     pygame.display.flip()
+    
+    dt = clock.tick(60) / 1000
   
   pygame.quit()
 
